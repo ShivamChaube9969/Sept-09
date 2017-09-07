460cd526e317a27ca72232c313030778eca4064e
ShivamChaube9969 <shivamkchaube@gmail.com>
2017-09-07 13:54:27 +0530

    Update findDuplicate.java

--- src/pack/findDuplicate.java
+++ src/pack/findDuplicate.java
@@ -2,15 +2,22 @@ package pack;
 
 public class findDuplicate {
 	public static void main(String[] args) {
-		int[] abc = new int[] { 10, 20, 20, 40, 50, 60 };
-		int count = 0;
-		for (int i = 0; i < abc.length; i++) {
+		int[] abc = new int[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 10 };
+		int count = 1;
+		first: for (int i = 0; i < abc.length - 1; i++) {
+
+			for (int k = i - 1; k >= 0; k--) {
+				if (abc[i] == abc[k])
+					continue first;
+			}
 			for (int j = i + 1; j < abc.length; j++) {
 				if (abc[i] == abc[j]) {
 					count++;
 				}
 			}
+			if (count > 1)
+				System.out.println(abc[i] + " is repeated " + count + " times");
+			count = 1;
 		}
-		System.out.println("count is : " + count);
 	}
 }
